fef3443a10cd133328cf8a1c4e809910a247fd7c
Hossein <139358833+HosseinnFazlii@users.noreply.github.com>
2024-06-11 21:19:23 +0330

    done

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,6 @@ ENV PYTHONUNBUFFERED=1
 WORKDIR /app
 COPY requirements.txt /app/
 RUN pip3 install --upgrade pip
-RUN pip3 install -r requirements.txt
+RUN pip3 install -r requirements.txt 
 COPY  ./monitoring  /app
 
--- bandwidth/admin.py
+++ bandwidth/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Server
+
+class ServerAdmin(admin.ModelAdmin):
+    list_display = ('hostname', 'ip_address', 'api_key', 'api_password','panel_ipaddress','vps_id','panel_port','telegramAPItoken','chat_ID1','chat_ID2')
+    search_fields = ('hostname', 'ip_address')
+
+admin.site.register(Server, ServerAdmin)
